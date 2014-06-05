f735c3d557db1606b94161453d867efad9c09089
naggappan <naggappan@gmail.com>
2014-06-05 18:52:26 +0530

    added filter for individual page

--- employee/management/commands/dbupdate.py
+++ employee/management/commands/dbupdate.py
@@ -6,11 +6,11 @@ class Command(NoArgsCommand):
     help = "prints hello world"
 
     def handle_noargs(self, **options):
-    	des = ["B1 US visa","H1B US visa","None","WP singapore"]
+    	des = ["B.E","B.Tech","MCA","MS","MSC","BSC","M.Tech","M.E","MBA"]
     	emp_ids = Employee.objects.filter().values('id')
     	for x in emp_ids:
     	 	emp_val = Employee.objects.get(id=x['id'])
-    	 	emp_val.Visa_Status = random.choice(des)
+    	 	emp_val.Qualification = random.choice(des)
     	 	# print emp_val.Visa_Status
     	 	emp_val.save()
         
