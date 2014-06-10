dde6bfac636e08ecd46675f4bc0a79b219760b63
naggappan <naggappan@gmail.com>
2014-06-10 10:13:22 +0530

    added choice for education qualification

--- employee/models.py
+++ employee/models.py
@@ -11,18 +11,19 @@ class Project(models.Model):
 		return u"%s" % (self.name)
 
 class Employee(models.Model):
+	qualif_choices = [('B.E','B.E'),('B.Tech','B.Tech'),('M.Tech','M.Tech'),('BSC','BSC'),('MCA','MCA'),('MS','MS'),('M.E','M.E'),('MS','MS'),('MSC','MSC'),('MBA','MBA')]
+	sub_choices = [('Computer Science','Computer Science'),('Information Technology','')]
 	id = models.IntegerField(max_length = 6,primary_key=True,verbose_name="Employee ID")
 	name = models.CharField(max_length = 100,verbose_name="Name")
 	dob = models.DateField(verbose_name='Date Of Birth')
 	doj = models.DateField(verbose_name='Date Of Joining')
 	exp = models.DecimalField(max_digits=4,decimal_places=2,verbose_name="Year of Experience")
 	Designation = models.CharField(max_length = 100)
-	Qualification = models.CharField(max_length = 100)
+	Qualification = models.CharField(max_length = 10,choices=qualif_choices)
+	# Major_Subject = models.CharField(max_length = 100,choices=sub_choices)
 	Visa_Status = models.CharField(max_length = 100)
 	Skill_sets = models.CharField(max_length = 100)
 	# Address = models.CharField(max_length = 200,blank=True)
-	# qualif_choices = ['BE','B.Tech','MBA','MS','MCA']
-	# qualif = models.CharField(max_length=10,choices=qualif,default=BE)
 	mobile=models.IntegerField(max_length = 12)
 	email = models.EmailField(max_length=50)
 	personal_email = models.EmailField(max_length=50,blank=True)
