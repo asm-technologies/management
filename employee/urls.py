5e6c616413771371e3eb1ad5a57a8814211b662a
naggappan <naggappan@gmail.com>
2014-06-25 15:36:39 +0530

    json response datatable

--- employee/urls.py
+++ employee/urls.py
@@ -7,10 +7,11 @@ from employee.views import *
 
 urlpatterns = patterns("",
 	url(r"^home/",'employee.views.home', name='home'),
+	url(r"^projects/project_json/(?P<proj_id>.+?)/$",'employee.views.project_json', name='project_json'),
 	url(r"^employee/$",'employee.views.employees_main', name='employees_main'),
 	url(r"^employee/(?P<emp_id>.+?)/$",'employee.views.employees', name='employees'),
 	url(r"^projects/$",'employee.views.projects_main', name='projects_main'),
-	url(r"^projects/(?P<proj_id>.+?)/$",'employee.views.projects', name='projects'),
+	
 
 )
 
--- employee/views.py
+++ employee/views.py
@@ -8,6 +8,33 @@ from django.http import HttpResponse
 
 from employee.models import *
 import datetime
+import json
+
+def project_json(request,proj_id):
+	projs = Project.objects.filter().values()
+	proj_values = []
+	for x in projs:
+		ID = x['id']
+		name = x['name']
+		description = x['description']
+		proj_values.append({"ID":ID,"name":name,"description":description})
+	if proj_id == 'null' :
+		first_proj = projs[0]
+		employees = Employee.objects.filter(proj=first_proj['id']).values()
+	else:
+		employees = Employee.objects.filter(proj=proj_id).values()
+	
+	response_list = []
+	for x in employees:
+				d1 =  datetime.date.today()
+				d2 =  x['doj']
+				yr_diff = (d1.year - d2.year)*12 + d1.month - d2.month
+				x['asm_exp'] = "%s.%s" % (yr_diff/12,yr_diff%12)
+				name = '<a href="javascript:poptastic(\'/employee/%d/\');">%s</a>' % (x['id'],x['name'])
+				response_list.append([x['id'],name,str(x['exp']),str(x['doj']),str(x['asm_exp']),str(x['start_date']),str(x['bill'])])
+	#response_data = {"aaData":[[1,'naggappan',1.5,'April 30, 2013',2.5,'April 2, 2014',True],[2,'naggappan',1.5,'April 30, 2013',2.5,'April 2, 2014',True]]}
+	response_data = {"aaData":response_list}
+	return HttpResponse(json.dumps(response_data), content_type="application/json")
 
 def employees_main(request):
 	all_employee = Employee.objects.filter().values()
@@ -19,8 +46,6 @@ def employees_main(request):
 def employees(request,emp_id):
 	employees = Employee.objects.filter(id=emp_id).values()
 	return render_to_response("employee.html",{"employee":"active",'employees':employees[0]},context_instance=RequestContext(request))
-	
-	
 
 def projects_main(request):
 	projs = Project.objects.filter().values()
@@ -30,25 +55,7 @@ def projects_main(request):
 		name = x['name']
 		description = x['description']
 		proj_values.append({"ID":ID,"name":name,"description":description})
-	first_proj = projs[0]
-	employees = Employee.objects.filter(proj=first_proj['id']).values()
-	for x in employees:
-				d1 =  datetime.date.today()
-				d2 =  x['doj']
-				yr_diff = (d1.year - d2.year)*12 + d1.month - d2.month
-				x['asm_exp'] = "%s.%s" % (yr_diff/12,yr_diff%12)
-	return render_to_response("projects_main.html",{"projects":"active","proj_values":proj_values,"first_proj":first_proj,"employees":employees},context_instance=RequestContext(request))
-
-def projects(request,proj_id):
-	proj = Project.objects.filter(id=proj_id).values()
-	proj_values = Project.objects.filter().values()
-	employees = Employee.objects.filter(proj=proj_id).values()
-	for x in employees:
-				d1 =  datetime.date.today()
-				d2 =  x['doj']
-				yr_diff = (d1.year - d2.year)*12 + d1.month - d2.month
-				x['asm_exp'] = "%s.%s" % (yr_diff/12,yr_diff%12)
-	return render_to_response("projects.html",{"projects":"active","proj":proj[0],"employees":employees,"proj_values":proj_values},context_instance=RequestContext(request))
+	return render_to_response("projects_main.html",{"projects":"active","proj_values":proj_values},context_instance=RequestContext(request))
 
 def home(request):
 	available_pjs = Project.objects.filter().values('id')
