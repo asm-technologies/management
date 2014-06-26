978a031602f04a1959ca7889833a7256fb85782b
naggappan <naggappan@gmail.com>
2014-06-26 12:13:29 +0530

    logout and groups removed in admin

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
 from eventlog.models import Log
 from account.models import Account, SignupCode, AccountDeletion, EmailAddress
+
 # Register your models here.
 
 admin.site.unregister(Log)
@@ -11,6 +13,7 @@ admin.site.unregister(Account)
 admin.site.unregister(SignupCode)
 admin.site.unregister(AccountDeletion)
 admin.site.unregister(EmailAddress)
+admin.site.unregister(Group)
 # admin.site.unregister( Sites )
 
 
