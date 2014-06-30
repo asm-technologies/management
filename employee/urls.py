4f5931b03c38af7a3a62fa3ac20e69a2fced1ea9
naggappan <naggappan@gmail.com>
2014-06-30 23:49:30 +0530

    url modified

--- employee/urls.py
+++ employee/urls.py
@@ -7,8 +7,8 @@ from employee.views import *
 
 urlpatterns = patterns("",
 	url(r"^home/",'employee.views.home', name='home'),
-	url(r"^projects/project_json/(?P<proj_id>.+?)/$",'employee.views.project_json', name='project_json'),
-	url(r"^employee/$",'employee.views.employees_main', name='employees_main'),
+	url(r"^project/project_json/(?P<proj_id>.+?)/$",'employee.views.project_json', name='project_json'),
+	url(r"^employees/$",'employee.views.employees_main', name='employees_main'),
 	url(r"^employee/(?P<emp_id>.+?)/$",'employee.views.employees', name='employees'),
 	url(r"^projects/$",'employee.views.projects_main', name='projects_main'),
 	
